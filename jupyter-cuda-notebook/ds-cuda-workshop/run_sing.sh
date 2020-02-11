0eb43c142957d78e23a58f515d8e20e71a851071
Joe Stubbs <jstubbs@tacc.utexas.edu>
2020-02-11 17:38:12 -0600

    make sure we're in ./home and explicitly set the NotebookApp.notebook_dir using a path via pwd.

--- jupyter-cuda-notebook/ds-cuda-workshop/run_sing.sh
+++ jupyter-cuda-notebook/ds-cuda-workshop/run_sing.sh
@@ -5,5 +5,9 @@
 #        cp /home/jupyter/jupyter_notebook_config.py $STOCKYARD/jupyter_notebook_config.py
 #        echo "\n\nimport os\nc.NotebookApp.notebook_dir = './home'" >> $STOCKYARD/jupyter_notebook_config.py
 #fi
+cd ~/
 mkdir ./home
-exec /usr/local/bin/jupyter notebook --notebook-dir=./home --ip=0.0.0.0 --port=8888
+cd ./home
+cp /home/jupyter/.jupyter/jupyter_notebook_config.py .
+printf "\n\nimport os\nc.NotebookApp.notebook_dir = \"$(pwd)\"" >> jupyter_notebook_config.py
+exec /usr/local/bin/jupyter notebook  --ip=0.0.0.0 --port=8888

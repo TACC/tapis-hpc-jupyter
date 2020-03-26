20785a748f76c70085e815f5185dd1a189decf6c
eliasmarkc <elias3c@gmail.com>
2020-03-26 13:14:05 -0500

    Update wrapper script

--- frontera/src/wrapper.sh
+++ frontera/src/wrapper.sh
@@ -9,28 +9,32 @@ LOCAL_IPY_PORT=8888
 
 export XDG_RUNTIME_DIR="$STOCKYARD/jupyter"
 
-module load tacc-singularity/3.4.2
-nohup singularity run '/work/projects/wma_apps/frontera/tapis-hpc-jupyter-assets/jupyter.simg' &
-
-# use ssh for port forwarding
-# echo Using ssh for port forwarding
-START_PORT=50000
 NODE_HOSTNAME_PREFIX=`hostname -s`
 NODE_HOSTNAME_DOMAIN=`hostname -d`
 NODE_HOSTNAME_LONG=`hostname -f`
 # on Frontera IPY_HOSTNAME_PREFIX has the form c###-###
-module load python3
-LOGIN_IPY_PORT=`python3 get_port.py $START_PORT $NODE_HOSTNAME_PREFIX`
-
-echo Port is $LOGIN_IPY_PORT
 
 for i in `seq 5`; do
     ssh -f -g -N -R $LOGIN_IPY_PORT:$NODE_HOSTNAME_LONG:$LOCAL_IPY_PORT login$i
 done
 
-# send email notification
-echo Your notebook is now running at http://$NODE_HOSTNAME_DOMAIN:$LOGIN_IPY_PORT with password $PASSWORD | mailx -s "Launch your Frontera HPC Jupyter notebook session" ${email}
+module load tacc-singularity/3.4.2
+module load python3
+BATCH_QUEUE="${AGAVE_JOB_BATCH_QUEUE}"
+if [ $BATCH_QUEUE == "rtx" ] || [ $BATCH_QUEUE == "rtx-dev" ]; then
+    START_PORT=45000
+    export LOGIN_IPY_PORT=`python3 get_port.py $START_PORT $NODE_HOSTNAME_PREFIX`
+    nohup singularity run --nv '/work/projects/wma_apps/frontera/tapis-hpc-jupyter-assets/ds-cuda-workshop.simg' &
+else
+    START_PORT=50000
+    export LOGIN_IPY_PORT=`python3 get_port.py $START_PORT $NODE_HOSTNAME_PREFIX`
+    nohup singularity run '/work/projects/wma_apps/frontera/tapis-hpc-jupyter-assets/jupyter-v2.img' &
+fi
 
+echo Port is $LOGIN_IPY_PORT
+
+# send email notification
+echo -e "Your notebook is now running at https://frontera-portal.tacc.utexas.edu/apps/jupyter/$LOGIN_IPY_PORT with password $PASSWORD\n\n\nThis message was auto-generated. If you'd like to contact us, don't reply to this email. Instead, please submit a ticket at https://frontera-portal.tacc.utexas.edu/tickets/new." | mailx -v -s "Launch your Frontera HPC Jupyter notebook session" -S smtp=smtp://relay.tacc.utexas.edu -S from="Frontera Apps <no-reply@frontera.tacc.utexas.edu>" ${email}
 # use file to kill job
 echo $NODE_HOSTNAME_LONG $IPYTHON_PID > $SESSION_FILE
 while [ -f $SESSION_FILE ] ; do
